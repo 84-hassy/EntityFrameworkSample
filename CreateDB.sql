4f71d00167f6448733e51ea40ca3e5e194ee5de8
Owner <test@test.com>
2025-11-18 08:44:13 +0900

    [add] 新規テーブル作成用のクエリを作成

---
+++ CreateDB.sql
@@ -0,0 +1,22 @@
+USE [EntityFrameworkDB]
+GO
+
+/****** Object:  Table [dbo].[products]    Script Date: 2025/11/18 8:43:04 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[products](
+	[id] [int] NOT NULL,
+	[name] [varchar](250) NOT NULL,
+	[price] [int] NOT NULL,
+ CONSTRAINT [PK_products] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
